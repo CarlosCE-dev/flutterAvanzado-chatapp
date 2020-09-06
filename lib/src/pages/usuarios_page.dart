f8cf7836834906b3cd21f4df835c8f95bb253913
CarlosCE-dev <c4r10z.n3t@gmail.com>
2020-09-06 10:07:13 -0600

    Diseño de usuarios y chat

--- lib/src/pages/usuarios_page.dart
+++ lib/src/pages/usuarios_page.dart
@@ -1,14 +1,89 @@
 import 'package:flutter/material.dart';
 
+// Model
+import 'package:chat_app/src/models/usuario.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class UsuariosPage extends StatelessWidget {
+class UsuariosPage extends StatefulWidget {
+
+  @override
+  _UsuariosPageState createState() => _UsuariosPageState();
+}
+
+class _UsuariosPageState extends State<UsuariosPage> {
+
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+  final usuarios = [
+    Usuario( uid: '1', nombre: 'Carlos', email: "test1@test.com", online: true ),
+    Usuario( uid: '2', nombre: 'Juan', email: "test2@test.com", online: true ),
+    Usuario( uid: '3', nombre: 'Christian', email: "test3@test.com", online: false ),
+    Usuario( uid: '4', nombre: 'Braulio', email: "test4@test.com", online: true ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('UsuariosPage'),
-     ),
+      appBar: AppBar(
+        title: Text('Mi Nombre', style: TextStyle( color: Colors.black87)),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon( Icons.exit_to_app, color: Colors.black87 ),
+          onPressed: (){},
+        ),
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.only( right :10 ),
+            child: Icon( Icons.check_circle, color: Colors.blue[400] ),
+          )
+        ],
+      ),
+      body: SmartRefresher(
+        controller: _refreshController,
+        enablePullDown: true,
+        onRefresh: _cargarUsuarios,
+        header: WaterDropHeader(
+          complete: Icon( Icons.check, color: Colors.blue[400]),
+          waterDropColor: Colors.blue[400],
+        ),
+        child: _listViewUsuarios(),
+      )
    );
+  }
+
+  ListView _listViewUsuarios() {
+    return ListView.separated(
+      physics: BouncingScrollPhysics(),
+      separatorBuilder: (_,i) => Divider(), 
+      itemCount: usuarios.length,
+      itemBuilder: (_,i) => _usuarioListTile( usuarios[i] ), 
+    );
+  }
+
+  ListTile _usuarioListTile( Usuario usuario ) {
+    return ListTile(
+        title: Text(usuario.nombre),
+        subtitle: Text(usuario.email),
+        leading: CircleAvatar(
+          child: Text( usuario.nombre.substring(0,2)),
+          backgroundColor: Colors.blue[100],
+        ),
+        trailing: Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+            color: usuario.online ? Colors.greenAccent : Colors.redAccent,
+            borderRadius: BorderRadius.circular(100)
+          ),
+        ),
+      );
+  }
+
+  void _cargarUsuarios() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
---
+++ lib/src/widgets/chat_message.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+
+  final String texto;
+  final String uid;
+  final AnimationController animationController;
+
+  const ChatMessage({
+    @required this.texto,
+    @required this.uid,
+    @required this.animationController
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: animationController,
+      child: SizeTransition(
+        sizeFactor: CurvedAnimation(
+          parent: animationController,
+          curve: Curves.easeOut
+        ),
+        child: Container(
+          child: this.uid == '123'
+            ? _myMessage() 
+            : _notMyMessage(),
+        ),
+      ),
+    );
+  }
+
+  Widget _myMessage() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        margin: EdgeInsets.only( bottom: 5, left: 50, right: 5 ),
+        padding: EdgeInsets.all(8.0),
+        child: Text(this.texto, style: TextStyle( color: Colors.white ),),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Color(0xff4d9ef6)
+        ),
+      ),
+    );
+  }
+
+  Widget _notMyMessage() {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        margin: EdgeInsets.only( bottom: 5, left: 5, right: 50 ),
+        padding: EdgeInsets.all(8.0),
+        child: Text(this.texto, style: TextStyle( color: Colors.black87 ),),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Color(0xffE4E5E8)
+        ),
+      ),
+    );
+  }
+}
